5196c14c3c5b8434101bc452453d9da84aecf3ae
kevwinzdesktop <kevcwinz@gmail.com>
2023-12-22 14:41:50 -0600

    Create AuthStack.js

---
+++ src/navigation/stacks/AuthStack.js
@@ -0,0 +1,12 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../../screens/LoginScreen";
+
+const Stack = createStackNavigator();
+
+export const AuthStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+    );
+};
